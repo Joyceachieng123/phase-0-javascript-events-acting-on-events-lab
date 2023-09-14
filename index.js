dfbf3c265646bc2622330c447a86527102893872
Joyce Achieng <joyce.achieng@student.moringaschool.com>
2023-09-14 23:15:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left) || 0;
+    const dodgerWidth = 40; 
+    const gameFieldWidth = 400; 
+  
+    const newLeft = left + 1;
+  
+    if (newLeft + dodgerWidth <= gameFieldWidth) {
+      dodger.style.left = `${newLeft}px`;
+    }
+  }
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
